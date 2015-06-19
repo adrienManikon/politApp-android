3edf03824c2bbed9c77a13a306b5b31acd0af9e0
adrienManikon <adrien.manikon@enyx.ch>
2015-06-19 15:49:37 +0200

    start design share button

--- app/src/main/java/enyx/ch/politapp/activity/MainActivity.java
+++ app/src/main/java/enyx/ch/politapp/activity/MainActivity.java
@@ -1,5 +1,12 @@
 package enyx.ch.politapp.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.os.Build;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
@@ -8,13 +15,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.ogaclejapan.arclayout.ArcLayout;
 import com.special.ResideMenu.ResideMenu;
 import com.special.ResideMenu.ResideMenuItem;
 
-import enyx.ch.politapp.R;
+import java.util.ArrayList;
+import java.util.List;
 
+import enyx.ch.politapp.R;
+import enyx.ch.politapp.utils.AnimatorUtils;
+import enyx.ch.politapp.widget.ClipRevealFrame;
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     private ResideMenu resideMenu;
@@ -27,6 +41,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private ResideMenuItem itemCalendar;
     private ResideMenuItem itemContact;
 
+    View rootLayout;
+    ClipRevealFrame menuLayout;
+    ArcLayout arcLayout;
+    View centerItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +53,24 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
         mContext = this;
         setUpMenu();
+        setUpShare();
+
+    }
+
+    private void setUpShare() {
+
+        rootLayout = (View) findViewById(R.id.root);
+        menuLayout = (ClipRevealFrame) findViewById(R.id.menu_layout);
+        arcLayout = (ArcLayout) findViewById(R.id.arc_layout);
+        centerItem = findViewById(R.id.center_item);
+
+        centerItem.setOnClickListener(this);
+
+        for (int i = 0, size = arcLayout.getChildCount(); i < size; i++) {
+            arcLayout.getChildAt(i).setOnClickListener(this);
+        }
+
+        findViewById(R.id.title_bar_right_share).setOnClickListener(this);
 
     }
 
@@ -70,9 +107,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         resideMenu.addMenuItem(itemQuizz, ResideMenu.DIRECTION_LEFT);
         resideMenu.addMenuItem(itemCalendar, ResideMenu.DIRECTION_LEFT);
         resideMenu.addMenuItem(itemContact, ResideMenu.DIRECTION_LEFT);
-
-        // You can disable a direction by setting ->
-        // resideMenu.setSwipeDirectionDisable(ResideMenu.DIRECTION_RIGHT);
 
         findViewById(R.id.title_bar_left_menu).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,7 +178,167 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 //            changeFragment(new SettingsFragment());
 //        }
 
+        switch (view.getId()) {
+            case R.id.title_bar_right_share:
+                onShareButtonClick(view);
+                return;
+        }
+
+        if (view instanceof Button) {
+            String text = "Clicked: " + ((Button) view).getText();
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        }
+
         resideMenu.closeMenu();
+    }
+
+    private void onShareButtonClick(View v) {
+
+        int x = (v.getLeft() + v.getRight()) / 2;
+        int y = (v.getTop() + v.getBottom()) / 2;
+        float radiusOfFab = 1f * v.getWidth() / 2f;
+        float radiusFromFabToRoot = (float) Math.hypot(
+                Math.max(x, rootLayout.getWidth() - x),
+                Math.max(y, rootLayout.getHeight() - y));
+
+        if (v.isSelected()) {
+            hideMenu(x, y, radiusFromFabToRoot, radiusOfFab);
+        } else {
+            showMenu(x, y, radiusOfFab, radiusFromFabToRoot);
+        }
+        v.setSelected(!v.isSelected());
+
+
+    }
+
+    private void showMenu(int cx, int cy, float startRadius, float endRadius) {
+        menuLayout.setVisibility(View.VISIBLE);
+
+        List<Animator> animList = new ArrayList<>();
+
+        Animator revealAnim = createCircularReveal(menuLayout, cx, cy, startRadius, endRadius);
+        revealAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+        revealAnim.setDuration(200);
+
+        animList.add(revealAnim);
+        animList.add(createShowItemAnimator(centerItem));
+
+        for (int i = 0, len = arcLayout.getChildCount(); i < len; i++) {
+            animList.add(createShowItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.playSequentially(animList);
+        animSet.start();
+    }
+
+    private void hideMenu(int cx, int cy, float startRadius, float endRadius) {
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = arcLayout.getChildCount() - 1; i >= 0; i--) {
+            animList.add(createHideItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        animList.add(createHideItemAnimator(centerItem));
+
+        Animator revealAnim = createCircularReveal(menuLayout, cx, cy, startRadius, endRadius);
+        revealAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+        revealAnim.setDuration(200);
+        revealAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                menuLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        animList.add(revealAnim);
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.playSequentially(animList);
+        animSet.start();
+
+    }
+
+    private Animator createCircularReveal(final ClipRevealFrame view, int x, int y, float startRadius,
+                                          float endRadius) {
+        final Animator reveal;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            reveal = ViewAnimationUtils.createCircularReveal(view, x, y, startRadius, endRadius);
+        } else {
+            view.setClipOutLines(true);
+            view.setClipCenter(x, y);
+            reveal = ObjectAnimator.ofFloat(view, "ClipRadius", startRadius, endRadius);
+            reveal.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    view.setClipOutLines(false);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+        }
+        return reveal;
+    }
+
+    private Animator createShowItemAnimator(View item) {
+        float dx = centerItem.getX() - item.getX();
+        float dy = centerItem.getY() - item.getY();
+
+        item.setScaleX(0f);
+        item.setScaleY(0f);
+        item.setTranslationX(dx);
+        item.setTranslationY(dy);
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.scaleX(0f, 1f),
+                AnimatorUtils.scaleY(0f, 1f),
+                AnimatorUtils.translationX(dx, 0f),
+                AnimatorUtils.translationY(dy, 0f)
+        );
+
+        anim.setInterpolator(new DecelerateInterpolator());
+        anim.setDuration(50);
+        return anim;
+    }
+
+    private Animator createHideItemAnimator(final View item) {
+        final float dx = centerItem.getX() - item.getX();
+        final float dy = centerItem.getY() - item.getY();
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.scaleX(1f, 0f),
+                AnimatorUtils.scaleY(1f, 0f),
+                AnimatorUtils.translationX(0f, dx),
+                AnimatorUtils.translationY(0f, dy)
+        );
+
+        anim.setInterpolator(new DecelerateInterpolator());
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                item.setTranslationX(0f);
+                item.setTranslationY(0f);
+            }
+        });
+        anim.setDuration(50);
+        return anim;
     }
 
     // What good method is to access resideMenu？
