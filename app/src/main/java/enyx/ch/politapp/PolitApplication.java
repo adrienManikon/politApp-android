65faf1bd66f1929d0c36f8e9e2d25edbeff25efa
adrienManikon <adrien.manikon@enyx.ch>
2015-06-25 11:37:42 +0200

    create app class

---
+++ app/src/main/java/enyx/ch/politapp/PolitApplication.java
@@ -0,0 +1,36 @@
+package enyx.ch.politapp;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by adrien.manikon on 25.06.15.
+ */
+public class PolitApplication extends Application {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+
+    public static boolean isAppInstalled(String uri) {
+
+        PackageManager packageManager = mContext.getPackageManager();
+
+        try {
+            packageManager.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
